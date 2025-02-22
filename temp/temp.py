29962598368aaf65be9b36aa68655f04e25ab04e
juliangrohmann <jgrohmann3@proton.me>
2025-02-22 05:59:45 -0500

    remove temp

--- temp/temp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np;
-
-data = np.load("clusters_r3_c200.npy")
-cutoff = data.shape[0] // 2
-s1 = data[:cutoff]
-s2 = data[cutoff:]
-np.save("clusters_r3_c200_p1.npy", s1)
-np.save("clusters_r3_c200_p2.npy", s2)
-print(f"{data[2000000000:2000000000+10]=}")
