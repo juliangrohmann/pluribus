c226794ff42ab6f3272a29706f14ea0a661f466a
juliangrohmann <jgrohmann3@proton.me>
2025-02-22 00:55:59 -0500

    add install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+apt update
+apt upgrade
+apt-get install libboost-all-dev
+apt install g++
+apt install cmake
+
+git clone https://github.com/catchorg/Catch2.git
+cd Catch2
+cmake -B build -S . -DBUILD_TESTING=OFF
+cmake --build build/ --target install
+cd ..
+
+git clone https://github.com/rogersce/cnpy.git
+cd cnpy
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ../..
+
+git clone https://github.com/juliangrohmann/pluribus.git
+cd pluribus
+mkdir build
+cd build
+cmake -DVERBOSE=OFF -DUNIT_TEST=ON ..
+cmake --build .
+
+# TODO: download clusters
+
+./Test
+./Benchmark
