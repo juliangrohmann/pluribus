ae36226dfbfc6db2230ff5f6da88fdc3fa8cbe4c
juliangrohmann <jgrohmann3@proton.me>
2025-07-26 08:56:13 +0700

    fix shared ptr

--- update_lossless/update_lossless.cpp
+++ update_lossless/update_lossless.cpp
@@ -6,7 +6,8 @@ using namespace pluribus;
 int main(int argc, char* argv[]) {
   LosslessBlueprint bp;
   cereal_load(bp, argv[1]);
-  auto tree_config = std::make_shared{ClusterSpec{169, 200, 200, 200}, ActionMode::make_blueprint_mode(bp.get_config().action_profile)};
+  const auto tree_config = std::make_shared<TreeStorageConfig>(ClusterSpec{169, 200, 200, 200},
+    ActionMode::make_blueprint_mode(bp.get_config().action_profile));
   bp.get_mutable_strategy()->set_config(tree_config);
   bp.get_mutable_strategy()->make_root();
   cereal_save(bp, std::string{"updated_"} + argv[1]);
