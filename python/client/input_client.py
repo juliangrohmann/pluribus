d6b3218c446e7dfeaeaccbf7e9855afd3b004a4c
juliangrohmann <jgrohmann3@proton.me>
2025-08-04 13:52:16 +0700

    add server arg to input client

--- python/client/input_client.py
+++ python/client/input_client.py
@@ -1,10 +1,16 @@
 import requests
+import argparse
 
-url = "http://5.39.216.171:8080/new_game"
-payload = {
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("server", type=str)
+  args = parser.parse_args()
+
+  url = f"http://{args.server}:8080/new_game"
+  payload = {
     "players": ["Alice", "Bob"],
     "stacks": [1000, 1000]
-}
+  }
 
-response = requests.post(url, json=payload)
-print(response.json())
+  response = requests.post(url, json=payload)
+  print(response.json())
