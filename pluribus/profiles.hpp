c2f4b9616632d6d6bb967487ab54e9d142b03450
juliangrohmann <jgrohmann3@proton.me>
2025-08-09 16:32:16 +0700

    remove simple profiles from header

--- pluribus/profiles.hpp
+++ pluribus/profiles.hpp
@@ -14,34 +14,14 @@ public:
   explicit HeadsUpBlueprintProfile(int stack_size);
 };
 
-class HeadsUpSimpleProfile : public ActionProfile {
-public:
-  explicit HeadsUpSimpleProfile(int stack_size);
-};
-
 class RingBlueprintProfile : public ActionProfile {
 public:
   explicit RingBlueprintProfile(int n_players);
 };
 
-class RingSimpleProfile : public ActionProfile {
-public:
-  explicit RingSimpleProfile(int n_players, int stack_size);
-};
-
 class WPTGoldRingBlueprintProfile : public ActionProfile {
 public:
   explicit WPTGoldRingBlueprintProfile(int n_players, bool fine_grained);
-};
-
-class WPTGoldRingSimpleProfile : public ActionProfile {
-public:
-  explicit WPTGoldRingSimpleProfile(int n_players);
-};
-
-class WPTGoldRingPreflopProfile : public ActionProfile {
-public:
-  explicit WPTGoldRingPreflopProfile(int n_players);
 };
 
 }
