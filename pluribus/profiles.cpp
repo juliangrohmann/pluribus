2d6361e1262f461fc20d82ced1efb58324876188
juliangrohmann <jgrohmann3@proton.me>
2025-08-09 18:04:58 +0700

    use smaller 2-bets

--- pluribus/profiles.cpp
+++ pluribus/profiles.cpp
@@ -113,8 +113,8 @@ RingBlueprintProfile::RingBlueprintProfile(const int n_players) : ActionProfile{
 
 WPTGoldRingBlueprintProfile::WPTGoldRingBlueprintProfile(const int n_players, const bool fine_grained) : ActionProfile{n_players} {
   // preflop RFI & isos
-  for(int pos = 0; pos < 3; ++pos) set_actions(single_size(0.85), 0, 1, pos);
-  for(int pos = 3; pos < n_players; ++pos) set_actions(single_size(0.52), 0, 1, pos);
+  for(int pos = 0; pos < 3; ++pos) set_actions(single_size(0.75), 0, 1, pos);
+  for(int pos = 3; pos < n_players; ++pos) set_actions(single_size(0.42), 0, 1, pos);
   set_iso_actions(single_size(1.50), 0, false);
   set_iso_actions(single_size(1.00), 0, true);
 
