3f1294884afb80656d5eb27b9a73146d7b6bb9d6
juliangrohmann <jgrohmann3@proton.me>
2025-08-09 15:53:43 +0700

    improve ring/wpt profiles

--- pluribus/profiles.cpp
+++ pluribus/profiles.cpp
@@ -7,9 +7,43 @@
 
 namespace pluribus {
 
+std::vector<Action> action_vec(const std::initializer_list<float>& action_list, const bool can_fold = true) {
+  std::vector<Action> actions;
+  if(can_fold) actions.push_back(Action::FOLD);
+  actions.push_back(Action::CHECK_CALL);
+  for(float bet_size : action_list) actions.emplace_back(bet_size);
+  actions.push_back(Action::ALL_IN);
+  return actions;
+}
+
+std::vector<Action> action_range(const float start, const float end, const float step, const bool can_fold = true) {
+  if(step <= 0.02f) Logger::error("Action range step is too small: " + std::to_string(step));
+  if(start <= end) Logger::error("Invalid action range: [" + join_as_strs(std::vector{start, end, step}, ", ") + "]");
+  std::vector<Action> actions;
+  if(can_fold) actions.push_back(Action::FOLD);
+  actions.push_back(Action::CHECK_CALL);
+  for(int i = 0; start + static_cast<float>(i) * step + 0.01f < end; ++i) {
+    actions.emplace_back(start + static_cast<float>(i) * step);
+  }
+  actions.emplace_back(end);
+  actions.push_back(Action::ALL_IN);
+  return actions;
+}
+
+std::vector<Action> single_size(const float bet_size, const bool can_fold = true) {
+  if(bet_size <= 0.0f) Logger::error("Bet size is too small: " + std::to_string(bet_size));
+  std::vector<Action> actions;
+  if(can_fold) actions.push_back(Action::FOLD);
+  actions.push_back(Action::CHECK_CALL);
+  actions.emplace_back(bet_size);
+  actions.push_back(Action::ALL_IN);
+  return actions;
+}
+
 BiasActionProfile::BiasActionProfile() : ActionProfile{1} {
   const std::vector bias_actions = {Action::BIAS_FOLD, Action::BIAS_CALL, Action::BIAS_RAISE, Action::BIAS_NONE};
-  set_iso_actions(bias_actions, 0);
+  set_iso_actions(bias_actions, 0, false);
+  set_iso_actions(bias_actions, 0, true);
   for(int round = 0; round <= 3; ++round) {
     set_actions(bias_actions, round, 0, 0);
   }
@@ -18,7 +52,8 @@ BiasActionProfile::BiasActionProfile() : ActionProfile{1} {
 HeadsUpBlueprintProfile::HeadsUpBlueprintProfile(const int stack_size) : ActionProfile{2} {
   // preflop RFI
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.75f}, Action::ALL_IN}, 0, 1, 0);
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action{2.00f}, Action::ALL_IN}, 0);
+  set_iso_actions(action_range(1.00f, 2.00f, 0.50f), 0, false);
+  set_iso_actions(action_range(1.00f, 2.00f, 0.50f), 0, true);
 
   // preflop 3-bet
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action{1.25f}, Action{1.50f}, Action{1.75f}, Action{2.00f}}, 0, 2, 0);
@@ -46,7 +81,8 @@ HeadsUpBlueprintProfile::HeadsUpBlueprintProfile(const int stack_size) : ActionP
 HeadsUpSimpleProfile::HeadsUpSimpleProfile(const int stack_size) : ActionProfile{2} {
   // preflop RFI
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.75f}, Action::ALL_IN}, 0, 1, 0);
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 0);
+  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 0, false);
+  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 0, true);
 
   // preflop 3-bet
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.50f}}, 0, 2, 0);
@@ -75,21 +111,23 @@ HeadsUpSimpleProfile::HeadsUpSimpleProfile(const int stack_size) : ActionProfile
 
 RingBlueprintProfile::RingBlueprintProfile(const int n_players, const int stack_size) : ActionProfile{n_players} {
   // preflop RFI & isos
-  for(int pos = 2; pos < n_players; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.60f}, Action::ALL_IN}, 0, 1, pos);
-  }
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.80f}, Action::ALL_IN}, 0, 1, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.80f}, Action::ALL_IN}, 0, 1, 1);
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action{1.50}, Action{2.00f}, Action{2.50f}, Action{3.00f}, Action::ALL_IN}, 0);
+  for(int pos = 0; pos < 2; ++pos) set_actions(single_size(0.80f), 0, 1, pos);
+  for(int pos = 2; pos < n_players; ++pos) set_actions(single_size(0.60f), 0, 1, pos);
+  set_iso_actions(action_range(1.00, 2.00, 0.50), 0, false);
+  set_iso_actions(action_vec({1.00, 1.50}), 0, true);
 
   // preflop 3-bet
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.10f}, Action{1.40f}, Action{1.70f}, Action::ALL_IN}, 0, 2, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action{1.30f}, Action{1.60f}, Action::ALL_IN}, 0, 2, 1);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.80f}, Action{1.00f}, Action{1.20f}, Action::ALL_IN}, 0, 2, 2);
+  for(int pos = 0; pos < 2; ++pos) {
+    set_actions(action_range(0.90, 1.90, 0.20), 0, 2, pos, false);
+    set_actions(action_range(0.60, 1.80, 0.20), 0, 2, pos, true);
+  }
+  for(int pos = 2; pos < n_players; ++pos) {
+    set_actions(action_range(0.90, 1.90, 0.20), 0, 2, pos, false);
+    set_actions(action_range(0.60, 1.20, 0.20), 0, 2, pos, true);
+  }
 
   // preflop 4-bet+
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.60f}, Action{0.70f}, Action{0.80f}, Action{0.90f}, Action{1.00f}, Action::ALL_IN}, 0, 3, 0);
-  if(stack_size < 10'000) add_action(Action{0.50f}, 0, 3, 0);
+  set_actions(action_range(0.50, 1.20, 0.10), 0, 3, 0);
 
   // flop
   set_actions({Action::CHECK_CALL, Action{0.33f}, Action{0.50f}, Action{0.75f}, Action{1.00f}, Action::ALL_IN}, 1, 0, 0);
@@ -113,7 +151,8 @@ RingSimpleProfile::RingSimpleProfile(const int n_players, const int stack_size) 
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.60f}, Action::ALL_IN}, 0, 1, n_players - 1);
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.80f}, Action::ALL_IN}, 0, 1, 0);
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.80f}, Action::ALL_IN}, 0, 1, 1);
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{0.85f}, Action::ALL_IN}, 0);
+  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.50f}, Action::ALL_IN}, 0, false);
+  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 0, false);
 
   // preflop 3-bet
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.40f}, Action::ALL_IN}, 0, 2, 0);
@@ -139,32 +178,23 @@ RingSimpleProfile::RingSimpleProfile(const int n_players, const int stack_size) 
 
 WPTGoldRingBlueprintProfile::WPTGoldRingBlueprintProfile(const int n_players, const int n_active) : ActionProfile{n_players} {
   // preflop RFI & isos
-  for(int pos = 0; pos < 3; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.85f}, Action::ALL_IN}, 0, 1, pos);
-  }
-  for(int pos = 3; pos < n_players; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.52f}, Action::ALL_IN}, 0, 1, pos);
-  }
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action{2.00f}, Action{3.00f}, Action::ALL_IN}, 0);
-  if(n_active <= 6) {
-    for(const Action a : {Action{1.50f}, Action{2.50f}}) {
-      add_iso_action(a, 0);
-    }
-  }
+  for(int pos = 0; pos < 3; ++pos) set_actions(single_size(0.85), 0, 1, pos);
+  for(int pos = 3; pos < n_players; ++pos) set_actions(single_size(0.52), 0, 1, pos);
+  set_iso_actions(action_range(1.00, 2.00, 0.50), 0, false);
+  set_iso_actions(action_vec({1.00, 1.50}), 0, true);
 
   // preflop 3-bet
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.83f}, Action{1.00f}, Action{1.17f}, Action::ALL_IN}, 0, 2, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.85f}, Action{1.025f}, Action{1.20f}, Action::ALL_IN}, 0, 2, 1, false);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.70f}, Action{0.875f}, Action{1.05f}, Action::ALL_IN}, 0, 2, 1, true);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.90f}, Action{1.075f}, Action{1.25f}, Action::ALL_IN}, 0, 2, 2, false);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.75f}, Action{0.935f}, Action{1.12f}, Action::ALL_IN}, 0, 2, 2, true);
+  for(int pos = 0; pos < 3; ++pos) {
+    set_actions(action_range(0.70, 1.45, 0.15), 0, 2, pos, false);
+    set_actions(action_range(0.50, 1.40, 0.15), 0, 2, pos, true);
+  }
   for(int pos = 3; pos < n_players; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.815f}, Action{1.00f}, Action{1.185f}, Action::ALL_IN}, 0, 2, pos, false);
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.65}, Action{0.815f}, Action{1.00f}, Action::ALL_IN}, 0, 2, pos, true);
+    set_actions(action_range(0.70, 1.45, 0.15), 0, 2, pos, false);
+    set_actions(action_range(0.55, 0.95, 0.10), 0, 2, pos, true);
   }
 
   // preflop 4-bet
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.45f}, Action{0.55f}, Action{0.65f}, Action::ALL_IN}, 0, 3, 0);
+  set_actions(action_range(0.40, 1.00, 0.10), 0, 3, 0);
 
   // flop
   set_actions({Action::CHECK_CALL, Action{0.50f}, Action{0.75f}, Action{1.00f}, Action::ALL_IN}, 1, 0, 0);
@@ -189,7 +219,8 @@ WPTGoldRingSimpleProfile::WPTGoldRingSimpleProfile(const int n_players) : Action
   for(int pos = 3; pos < n_players; ++pos) {
     set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.52f}, Action::ALL_IN}, 0, 1, pos);
   }
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{0.70f}, Action::ALL_IN}, 0  );
+  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{0.70f}, Action::ALL_IN}, 0, false);
+  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{0.70f}, Action::ALL_IN}, 0, true);
 
   // preflop 3-bet
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 0, 2, 0);
@@ -209,43 +240,6 @@ WPTGoldRingSimpleProfile::WPTGoldRingSimpleProfile(const int n_players) : Action
   // flop
   set_actions({Action::CHECK_CALL, Action{0.50f}, Action{0.75f}, Action{1.00f}, Action::ALL_IN}, 1, 0, 0);
   set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.50f}, Action{1.00f}, Action::ALL_IN}, 1, 1, 0);
-
-  // turn
-  set_actions({Action::CHECK_CALL, Action{0.50f}, Action{1.00f}, Action::ALL_IN}, 2, 0, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 2, 1, 0);
-
-  // river
-  set_actions({Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 3, 0, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 3, 1, 0);
-}
-
-WPTGoldRingPreflopProfile::WPTGoldRingPreflopProfile(const int n_players) : ActionProfile{n_players} {
-  // preflop RFI & isos
-  for(int pos = 0; pos < 3; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.85f}, Action::ALL_IN}, 0, 1, pos);
-  }
-  for(int pos = 3; pos < n_players; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.52f}, Action::ALL_IN}, 0, 1, pos);
-  }
-  set_iso_actions({Action::FOLD, Action::CHECK_CALL, Action{0.70f}, Action::ALL_IN}, 0);
-
-  // preflop 3-bet
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.83f}, Action{1.00f}, Action{1.17f}, Action::ALL_IN}, 0, 2, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.85f}, Action{1.025f}, Action{1.20f}, Action::ALL_IN}, 0, 2, 1, false);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.70f}, Action{0.875f}, Action{1.05f}, Action::ALL_IN}, 0, 2, 1, true);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.90f}, Action{1.075f}, Action{1.25f}, Action::ALL_IN}, 0, 2, 2, false);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.75f}, Action{0.935f}, Action{1.12f}, Action::ALL_IN}, 0, 2, 2, true);
-  for(int pos = 3; pos < n_players; ++pos) {
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.815f}, Action{1.00f}, Action{1.185f}, Action::ALL_IN}, 0, 2, pos, false);
-    set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.65}, Action{0.815f}, Action{1.00f}, Action::ALL_IN}, 0, 2, pos, true);
-  }
-
-  // preflop 4-bet
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{0.45f}, Action{0.55f}, Action{0.65f}, Action::ALL_IN}, 0, 3, 0);
-
-  // flop
-  set_actions({Action::CHECK_CALL, Action{0.50f}, Action{1.00f}, Action::ALL_IN}, 1, 0, 0);
-  set_actions({Action::FOLD, Action::CHECK_CALL, Action{1.00f}, Action::ALL_IN}, 1, 1, 0);
 
   // turn
   set_actions({Action::CHECK_CALL, Action{0.50f}, Action{1.00f}, Action::ALL_IN}, 2, 0, 0);
