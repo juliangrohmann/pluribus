9320a2f5aeecb7f4a46fd1a474f7e7b9468fb816
juliangrohmann <jgrohmann3@proton.me>
2025-08-15 16:15:49 +0700

    add debug logging

--- pluribus/earth_movers_dist.cpp
+++ pluribus/earth_movers_dist.cpp
@@ -89,8 +89,9 @@ std::unordered_map<hand_index_t, int> build_cluster_map(const std::vector<hand_i
 std::vector<int> build_histogram(const hand_index_t turn_idx, const std::unordered_map<hand_index_t, int>& cluster_map) {
   constexpr int round = 2;
   uint8_t cards[7];
+  std::cout << "Turn index: " << turn_idx << "\n";
   HandIndexer::get_instance()->unindex(turn_idx, cards, round);
-  std::cout << "Hand: " << cards_to_str(cards, 6);
+  std::cout << "Hand: " << cards_to_str(cards, 6) << "\n";
   const uint64_t mask = card_mask(cards, n_board_cards(round) + 2);
   std::vector<int> histogram;
   for(uint8_t card = 0; card < MAX_CARDS; ++card) {
