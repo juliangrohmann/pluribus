67764f7dbae070532f5b24a9c32c7438c254f18f
juliangrohmann <juliangrohmann@protonmail.com>
2025-08-23 17:32:32 +0700

    split site information into site config

--- python/client/interface.py
+++ python/client/interface.py
@@ -25,112 +25,110 @@ win_default_offset = 8
 win_header_offset = 1
 win_header_h = 30
 
-def build_card_mask() -> np.ndarray:
+def _build_card_mask() -> np.ndarray:
   with open(ocr.charset_path, "r", encoding="utf-8") as f:
     charset = [line.rstrip("\n") for line in f]
   return np.array([i == 0 or c in "1234567890JKQA" for i,c in enumerate(charset)])
 
-card_mask = build_card_mask()
+card_mask = _build_card_mask()
 
-def assert_player_pos(i:int) -> None:
+def _assert_player_pos(i:int) -> None:
   assert 0 <= i <= 5, f"Invalid Player Position: {player_pos}"
 
-def frac_to_pix(x:float, y:float, img_w:int, img_h:int) -> Tuple[int,int]:
+def _frac_to_pix(x:float, y:float, img_w:int, img_h:int) -> Tuple[int,int]:
   return int(round(x * img_w)), int(round(y * (img_h - win_header_h) + win_header_h))
 
-def crop(img:Image, box:Tuple[int,int,int,int]) -> Image:
-  return img.crop((*frac_to_pix(box[0], box[1], img.width, img.height), *frac_to_pix(box[2], box[3], img.width, img.height)))
+def _crop(img:Image, box:Tuple[int,int,int,int]) -> Image:
+  return img.crop((*_frac_to_pix(box[0], box[1], img.width, img.height), *_frac_to_pix(box[2], box[3], img.width, img.height)))
+
+def _apply_color_filter(pos:int, img:Image, coords:Tuple[Tuple[float,float], ...], fun:Callable[[Tuple[int,int,int]], bool]) -> bool:
+  _assert_player_pos(pos)
+  return fun(img.getpixel(_frac_to_pix(*coords[pos], img.width, img.height)))
+
+def _parse_ocr(img:Image, coords:Tuple[int,int,int,int], repl:Tuple[str, ...]=tuple(), expand: int=4, mask:np.ndarray|None=None, debug_label:str=None) -> str:
+  raw = ocr.pipeline.run(cropped := _crop(img, coords), expand=expand, mask=mask)[0]
+  if debug_label:
+    cropped.save(f"img_debug\\{debug_label}.png")
+  for s in repl: raw = raw.replace(s, '')
+  return raw
+
+def _parse_rank(img:Image, coords:Tuple[int,int,int,int], n:int=8, debug_label:str=None) -> str:
+  cropped = _crop(img, coords)
+  repeated = PIL.Image.new(cropped.mode, (cropped.width * n, cropped.height))
+  for i in range(n): repeated.paste(cropped, (i * cropped.width, 0))
+  counter = Counter(ocr.pipeline.run(repeated, expand=10, mask=card_mask)[0])
+  if debug_label:
+    repeated.save(f"img_debug\\{debug_label}.png")
+  return 'T' if set(e[0] for e in counter.most_common(2)) == set('10') else counter.most_common(1)[0][0]
 
 class PokerTable:
   def __init__(self, wnd_handle:int, conf:dict) -> None:
     self.handle = wnd_handle
     self.config = conf
 
-  def hand(self, pos:int, img:Image) -> str|None:
-    assert_player_pos(pos)
-    return hand_str if (hand_str := self._parse_cards(img, self.config.hole_cards_ranks[pos], self.config.hole_cards_suits[pos], debug_label=f"hole_cards_{pos}_rank")) else None
-
-  def username(self, img:Image, pos:int) -> str|None:
-    assert_player_pos(pos)
-    name = self._parse_ocr(img, self.config.usernames[pos], tuple(), debug_label=f"username_{pos}")
-    return name if name not in ["Bet", "Raise", "Check", "Call", "Fold"] else None
-
-  def street(self, img:Image) -> str|None: return {0:"Preflop",3:"Flop",4:"Turn",5:"River"}.get(sum(bool(self._parse_suit(img,self.config.board_suits[i])) for i in range(5)), None)
-  def board(self, img:Image) -> str|None: return self._parse_cards(img, self.config.board_ranks, self.config.board_suits, debug_label=f"board_rank")
-  def stack_size(self, img:Image, pos:int, blinds:bool=False) -> float|None: return self._cash_amount_by_pos(pos, img, self.config.stacks, blinds=blinds, debug_label=f"stack_size_{pos}")
-  def bet_size(self, img:Image, pos:int, blinds:bool=False) -> float|None: return self._cash_amount_by_pos(pos, img, self.config.bet_size, blinds=blinds, debug_label=f"bet_size_{pos}")
-  def pot_size(self, img:Image, blinds:bool=False) -> float|None: return self._cash_amount(img, self.config.pot, ("Pot", " ", ":"), blinds, debug_label="pot_size")
-  def active_pos(self, img:Image) -> int|None: return self._find_pos(img, self.config.active, self.config.is_active)
-  def button_pos(self, img:Image) -> int|None: return self._find_pos(img, self.config.button, self.config.has_button)
-  def is_seat_open(self, img:Image, pos:int) -> bool: return self._apply_color_filter(pos, img, self.config.seats, self.config.is_seat_open)
-  def is_sitting_out(self, img:Image, pos:int) -> bool: return self._parse_ocr(img, self.config.stacks[pos], debug_label=f"sitout_{pos}") == "Sitting Out"
-  def has_cards(self, img:Image, pos:int) -> bool: return self._apply_color_filter(pos, img, self.config.cards, self.config.has_cards)
-  def has_bet(self, img:Image, pos:int) -> bool: return self._apply_color_filter(pos, img, self.config.bet_chips, self.config.has_bet)
-  def screenshot(self) -> Image: return pyautogui.screenshot(region=self.rect())
-  def title(self) -> str: return win32gui.GetWindowText(self.handle)
-  def stakes(self) -> Tuple[float,float]: return self.config.get_stakes(self.title())
-  def move(self, x:int, y:int, w:int, h:int) -> None: win32gui.SetWindowPos(self.handle, win32con.HWND_TOP, x, y, w, h, 0)
-  def __repr__(self) -> str: return f"<PokerTable title=\"{self.title()}\", config=\"{self.config.name}\", handle={self.handle}>"
-  def __str__(self) -> str: return self.__repr__()
-
   def rect(self) -> Tuple[int,int,int,int]:
     r = win32gui.GetWindowRect(self.handle)
     return r[0] + win_default_offset, r[1] + win_header_offset, r[2] - r[0] - 2 * win_default_offset, r[3] - r[1] - win_default_offset - win_header_offset
 
-  def _apply_color_filter(self, pos:int, img:Image, coords:Tuple[Tuple[float,float], ...], fun:Callable[[Tuple[int,int,int]], bool]) -> bool:
-    assert_player_pos(pos)
-    if img is None: img = self.screenshot()
-    return fun(img.getpixel(frac_to_pix(*coords[pos], img.width, img.height)))
+  def hand(self, pos:int, img:Image) -> str|None:
+    _assert_player_pos(pos)
+    return hand_str if (hand_str := self._parse_cards(img, self.config.hole_cards_ranks[pos], self.config.hole_cards_suits[pos], debug_label=f"hole_cards_{pos}_rank")) else None
+
+  def username(self, img:Image, pos:int) -> str|None:
+    _assert_player_pos(pos)
+    name = _parse_ocr(img, self.config.usernames[pos], tuple(), debug_label=f"username_{pos}")
+    return name if name not in ["Bet", "Raise", "Check", "Call", "Fold"] else None
+
+  def street(self, img:Image) -> str|None:
+    return {0: "Preflop", 3: "Flop", 4: "Turn", 5: "River"}.get(sum(bool(self._parse_suit(img, self.config.site.board_suits[i])) for i in range(5)), None)
+
+  def board(self, img:Image) -> str|None: return self._parse_cards(img, self.config.site.board_ranks, self.config.site.board_suits, debug_label=f"board_rank")
+  def stack_size(self, img:Image, pos:int, blinds:bool=False) -> float|None: return self._cash_amount_by_pos(pos, img, self.config.stacks, blinds=blinds, debug_label=f"stack_size_{pos}")
+  def bet_size(self, img:Image, pos:int, blinds:bool=False) -> float|None: return self._cash_amount_by_pos(pos, img, self.config.bet_size, blinds=blinds, debug_label=f"bet_size_{pos}")
+  def pot_size(self, img:Image, blinds:bool=False) -> float|None: return self._cash_amount(img, self.config.site.pot, ("Pot", " ", ":"), blinds, debug_label="pot_size")
+  def active_pos(self, img:Image) -> int|None: return self._find_pos(img, self.config.active, self.config.site.is_active)
+  def button_pos(self, img:Image) -> int|None: return self._find_pos(img, self.config.button, self.config.site.has_button)
+  def is_seat_open(self, img:Image, pos:int) -> bool: return _apply_color_filter(pos, img, self.config.seats, self.config.site.is_seat_open)
+  def is_sitting_out(self, img:Image, pos:int) -> bool: return _parse_ocr(img, self.config.stacks[pos], debug_label=f"sitout_{pos}") == "Sitting Out"
+  def has_cards(self, img:Image, pos:int) -> bool: return _apply_color_filter(pos, img, self.config.cards, self.config.site.has_cards)
+  def has_bet(self, img:Image, pos:int) -> bool: return _apply_color_filter(pos, img, self.config.bet_chips, self.config.site.has_bet)
+  def screenshot(self) -> Image: return pyautogui.screenshot(region=self.rect())
+  def title(self) -> str: return win32gui.GetWindowText(self.handle)
+  def stakes(self) -> Tuple[float,float]: return self.config.site.get_stakes(self.title())
+  def move(self, x:int, y:int, w:int, h:int) -> None: win32gui.SetWindowPos(self.handle, win32con.HWND_TOP, x, y, w, h, 0)
+  def __repr__(self) -> str: return f"<PokerTable title=\"{self.title()}\", config=\"{self.config.name}\", handle={self.handle}>"
+  def __str__(self) -> str: return self.__repr__()
 
   def _find_pos(self, img:Image, coords:Tuple[Tuple[float,float], ...], fun:Callable[[Tuple[int,int,int]], bool]) -> int|None:
-    if img is None: img = self.screenshot()
     for pos in range(self.config.n_players):
-      if self._apply_color_filter(pos, img, coords, fun): return pos
+      if _apply_color_filter(pos, img, coords, fun): return pos
     return None
 
-  def _parse_ocr(self, img:Image, coords:Tuple[int,int,int,int], repl:Tuple[str, ...]=tuple(), expand: int=4, mask:np.ndarray|None=None, debug_label:str=None) -> str:
-    if img is None: img = self.screenshot()
-    raw = ocr.pipeline.run(cropped := crop(img, coords), expand=expand, mask=mask)[0]
-    if debug_label:
-      cropped.save(f"img_debug\\{debug_label}.png")
-    for s in repl: raw = raw.replace(s, '')
-    return raw
-
-  def _parse_rank(self, img:Image, coords:Tuple[int,int,int,int], n:int=8, debug_label:str=None) -> str:
-    if img is None: img = self.screenshot()
-    cropped = crop(img, coords)
-    repeated = PIL.Image.new(cropped.mode, (cropped.width * n, cropped.height))
-    for i in range(n): repeated.paste(cropped, (i * cropped.width, 0))
-    counter = Counter(ocr.pipeline.run(repeated, expand=10, mask=card_mask)[0])
-    if debug_label:
-      repeated.save(f"img_debug\\{debug_label}.png")
-    return 'T' if set(e[0] for e in counter.most_common(2)) == set('10') else counter.most_common(1)[0][0]
-
   def _cash_amount(self, img:Image, coords:Tuple[int,int,int,int], repl:Tuple[str, ...]=tuple(), blinds:bool=False, debug_label:str=None) -> float|None:
-    raw = self._parse_ocr(img, coords, repl + ("$", ","), debug_label=debug_label)
+    raw = _parse_ocr(img, coords, repl + ("$", ","), debug_label=debug_label)
     if raw.lower() == "all in": return 0.0
     try:
       amount = float(raw)
-      return amount / self.get_stakes()[1] if blinds else amount
+      return amount / self.stakes()[1] if blinds else amount
     except ValueError:
       return None
 
   def _cash_amount_by_pos(self, pos:int, img:Image, coords_by_pos:Tuple[Tuple[int,int,int,int], ...], blinds:bool=False, debug_label:str=None) -> float|None:
-    assert_player_pos(pos)
+    _assert_player_pos(pos)
     return self._cash_amount(img, coords_by_pos[pos], blinds=blinds, debug_label=debug_label)
 
   def _rgb_to_suit(self, rgb:Tuple[int,int,int]) -> str|None:
-    if self.config.is_heart(rgb): return 'h'
-    if self.config.is_club(rgb): return 'c'
-    if self.config.is_diamond(rgb): return 'd'
-    if self.config.is_spade(rgb): return 's'
+    if self.config.site.is_heart(rgb): return 'h'
+    if self.config.site.is_club(rgb): return 'c'
+    if self.config.site.is_diamond(rgb): return 'd'
+    if self.config.site.is_spade(rgb): return 's'
     return None
 
   def _parse_suit(self, img:Image, coords:Tuple[int,int]) -> str|None:
-    return self._rgb_to_suit(img.getpixel(frac_to_pix(*coords, *img.size)))
+    return self._rgb_to_suit(img.getpixel(_frac_to_pix(*coords, *img.size)))
 
   def _parse_cards(self, img:Image, rank_coords:Tuple[Tuple[int, int, int, int], ...], suit_coords:Tuple[Tuple[int,int], ...], debug_label:str=None) -> str|None:
-    return ''.join(self._parse_rank(img, rank_coords[i], debug_label=f"{debug_label}_{i}") + suit for i in range(len(rank_coords))
+    return ''.join(_parse_rank(img, rank_coords[i], debug_label=f"{debug_label}_{i}") + suit for i in range(len(rank_coords))
                    if (suit := self._parse_suit(img, suit_coords[i])) is not None)
 
 def is_real_window(h_wnd:int) -> bool:
@@ -148,7 +146,7 @@ def get_tables() -> List[PokerTable]:
 def get_config(handle:int) -> dict|None:
   title = win32gui.GetWindowText(handle)
   for c in config.configs:
-    if c.get_stakes(title) is not None: return c
+    if c.site.get_stakes(title) is not None: return c
   return None
 
 def debug() -> None:
