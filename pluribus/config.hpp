a15f5db299a62b14008d3dde889a7d443bfc028f
juliangrohmann <jgrohmann3@proton.me>
2025-08-27 06:32:48 +0700

    increase rt solver default it/sec to 1M

--- pluribus/config.hpp
+++ pluribus/config.hpp
@@ -92,7 +92,7 @@ struct RealTimeTimingConfig {
 };
 
 struct RealTimeSolverConfig : DiscountConfig {
-  explicit RealTimeSolverConfig(const RealTimeTimingConfig& timings = RealTimeTimingConfig{}, long it_per_sec = 100'000);
+  explicit RealTimeSolverConfig(const RealTimeTimingConfig& timings = RealTimeTimingConfig{}, long it_per_sec = 1'000'000);
 
   std::string to_string() const;
   void set_iterations(const RealTimeTimingConfig& timings, long it_per_sec);
