92c18a123c0b4deb7215481d3933342d055be479
juliangrohmann <jgrohmann3@proton.me>
2025-08-27 15:49:49 +0700

    remove contineu

--- pluribus/mccfr.cpp
+++ pluribus/mccfr.cpp
@@ -95,7 +95,6 @@ void MCCFRSolver<StorageT>::_solve(long t_plus) {
       thread_local omp::HandEvaluator eval;
       thread_local Board board;
       thread_local MarginalRejectionSampler sampler{get_config().init_ranges, get_config().init_board, get_config().dead_ranges};
-      if(is_interrupted()) continue;
       if(is_debug) Logger::log("============== t = " + std::to_string(t) + " ==============");
       if(should_log(t)) {
         #pragma omp cancellation point for
